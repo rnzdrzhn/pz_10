1bb4fe79f7d2b5823b462cc464bd93868bc0400a
Арина Задорожняя <zadoroozhnyayaa@gmail.com>
2022-11-03 08:05:09 +0300

    Добавьте файлы проекта.

---
+++ pz_10/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace pz_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение переменной N, N > 0");
+            string S = "some text";
+            int N;
+            N = Convert.ToInt32(Console.ReadLine());
+
+            if (S.Length > N)
+            {
+                Console.WriteLine(S.Remove(0, S.Length - N));
+            }
+            else if (S.Length < N)
+            {
+                Console.WriteLine(S.PadLeft(N - S.Length, '.'));
+            }
+        }
+    }
+}
